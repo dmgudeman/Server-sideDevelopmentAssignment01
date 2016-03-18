b78aa86777894a22b8451c1fa9afd123859a7c29
David C gudeman <dcgudeman@gmail.com>
2016-03-18 09:41:44 -0700

    fixed module.exports issue

--- dishRouter01c.js
+++ dishRouter01c.js
@@ -1,22 +1,22 @@
 var express = require('express');
-//var dishRouter = express.Router();
-//dishRouter.use(bodyParser.json());
+var bodyParser = require('body-parser');
+var dishRouter = express.Router();
+
+dishRouter.use(bodyParser.json());
 
 dishRouter.route('/')
+  .all(function (req, res, next) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    next();
+  })
+  .get(function (req, res, next) {
+    res.end('Will send all the dishes to you!');
+  })
+  .post(function (req, res, next) {
+    res.end('Will add the dish: ' + req.body.name + ' with details: ' + req.body.description);
+  })
+  .delete(function (req, res, next) {
+    res.end('Deleting all dishes');
+  });
 
-    .all(function(req,res,next) {
-        res.writeHead(200, { 'Content-Type': 'text/plain'});
-        next();
-    })
-
-    .get( function(req,res,next) {
-        res.end('Will send all the dishes to you!');
-    })
-
-    .post(function(req,res,next) {
-        res.end('Will add the dish: ' + req.body.name + ' with details: ' + req.body.description);
-    })
-
-    .delete(function(req,res,next) {
-        res.end('Deleting all dishes');
-    });
+module.exports = dishRouter;
